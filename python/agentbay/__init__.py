e955463ff59203a0dac8c4c78437a1ea3528e735
xiangting.cxt <xiangting.cxt@alibaba-inc.com>
2025-06-16 17:21:46 +0800

    fix:export modules in application

--- python/agentbay/__init__.py
+++ python/agentbay/__init__.py
@@ -6,6 +6,6 @@ from .oss import Oss
 from .filesystem import FileSystem
 from .window import Window
 from .command import Command
+from .application import ApplicationManager, InstalledApp, Process
 
-
-__all__ = ["AgentBay", "Session", "AgentBayError", "AuthenticationError", "APIError", "UI", "Oss", "FileSystem", "Window", "Command"]
+__all__ = ["AgentBay", "Session", "AgentBayError", "AuthenticationError", "APIError", "UI", "Oss", "FileSystem", "Window", "Command", "ApplicationManager", "InstalledApp", "Process"]
