4e394116e8a5032ef34ffabbcb08800bd6fde5a0
xiangting.cxt <xiangting.cxt@alibaba-inc.com>
2025-07-09 17:11:02 +0800

    fix:workflow test

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,12 +1,14 @@
 // Export all public classes and interfaces
 export { AgentBay } from "./agent-bay";
-export { Session } from "./session";
+export * from "./api";
+export * from "./application";
+export * from "./command";
 export { Context, ContextService } from "./context";
 export * from "./exceptions";
 export * from "./filesystem";
-export * from "./command";
 export * from "./oss";
-export * from "./api";
-export * from "./application";
-export * from "./ui";
+export { Session } from "./session";
 export { type ListSessionParams } from "./types";
+export * from "./ui";
+// Export utility functions
+export { log, logError } from "./utils/logger";
