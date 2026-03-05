d2ae3e660a48cea8cca412fda52c6995bd13c9b3
shiwen.lc <shiwen.lc@alibaba-inc.com>
2026-03-05 11:53:10 +0800

    feat: add java examples for captcha solving
    to #78278505
    
    Signed-off-by: shiwen.lc <shiwen.lc@alibaba-inc.com>

---
+++ java/agentbay/src/main/java/com/aliyun/agentbay/browser/BrowserNotifyMessage.java
@@ -0,0 +1,125 @@
+package com.aliyun.agentbay.browser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Browser notify message for SDK and sandbox communication, such as call-for-user messages.
+ * 
+ * <p>Example usage:</p>
+ * <pre>{@code
+ * BrowserNotifyMessage notifyMsg = new BrowserNotifyMessage(
+ *     "call-for-user",
+ *     3,
+ *     201,
+ *     "captcha solving start",
+ *     "pause",
+ *     Map.of("max_wait_time", 30)
+ * );
+ * }</pre>
+ */
+public class BrowserNotifyMessage {
+    private String type;
+    private Integer id;
+    private Integer code;
+    private String message;
+    private String action;
+    private Map<String, Object> extraParams;
+
+    /**
+     * Constructs a new BrowserNotifyMessage with all parameters.
+     * 
+     * @param type Type of the notification (e.g., "call-for-user")
+     * @param id ID of the notification
+     * @param code Status code of the notification (e.g., 201)
+     * @param message Descriptive message (e.g., "captcha solving start")
+     * @param action Action to be taken (e.g., "pause", "resume", "takeover", "takeoverdone")
+     * @param extraParams Additional parameters as a map (e.g., {"max_wait_time": 30})
+     */
+    public BrowserNotifyMessage(String type, Integer id, Integer code, String message, String action, Map<String, Object> extraParams) {
+        this.type = type;
+        this.id = id;
+        this.code = code;
+        this.message = message;
+        this.action = action;
+        this.extraParams = extraParams != null ? extraParams : new HashMap<>();
+    }
+
+    /**
+     * Constructs a new BrowserNotifyMessage with empty extraParams.
+     */
+    public BrowserNotifyMessage(String type, Integer id, Integer code, String message, String action) {
+        this(type, id, code, message, action, new HashMap<>());
+    }
+
+    /**
+     * Convert BrowserNotifyMessage to a Map for serialization.
+     * 
+     * @return Map representation of the notify message
+     */
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        if (type != null) map.put("type", type);
+        if (id != null) map.put("id", id);
+        if (code != null) map.put("code", code);
+        if (message != null) map.put("message", message);
+        if (action != null) map.put("action", action);
+        if (extraParams != null && !extraParams.isEmpty()) {
+            map.put("extra_params", extraParams);
+        }
+        return map;
+    }
+
+    /**
+     * Create a BrowserNotifyMessage from a Map.
+     * 
+     * @param map Map containing the notify message data
+     * @return BrowserNotifyMessage instance
+     */
+    @SuppressWarnings("unchecked")
+    public static BrowserNotifyMessage fromMap(Map<String, Object> map) {
+        if (map == null) {
+            return null;
+        }
+        
+        String type = (String) map.get("type");
+        Integer id = map.get("id") instanceof Number ? ((Number) map.get("id")).intValue() : null;
+        Integer code = map.get("code") instanceof Number ? ((Number) map.get("code")).intValue() : null;
+        String message = (String) map.get("message");
+        String action = (String) map.get("action");
+        Map<String, Object> extraParams = (Map<String, Object>) map.get("extra_params");
+        
+        return new BrowserNotifyMessage(type, id, code, message, action, extraParams);
+    }
+
+    // Getters and Setters
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    
+    public Integer getCode() { return code; }
+    public void setCode(Integer code) { this.code = code; }
+    
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+    
+    public String getAction() { return action; }
+    public void setAction(String action) { this.action = action; }
+    
+    public Map<String, Object> getExtraParams() { return extraParams; }
+    public void setExtraParams(Map<String, Object> extraParams) { this.extraParams = extraParams; }
+
+    @Override
+    public String toString() {
+        return "BrowserNotifyMessage{" +
+                "type='" + type + '\'' +
+                ", id=" + id +
+                ", code=" + code +
+                ", message='" + message + '\'' +
+                ", action='" + action + '\'' +
+                ", extraParams=" + extraParams +
+                '}';
+    }
+}
